8cf665f4141633d74568899d8d71d82c91d6b22c
Andrei Bacescu <bacescuandrei@gmail.com>
2017-04-25 15:20:35 +0300

    Commit 1

--- src/main/java/net/metrosystems/designpattern/DemoApplication.java
+++ src/main/java/net/metrosystems/designpattern/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+package net.metrosystems.designpattern;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
