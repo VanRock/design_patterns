5c9375f69d07fbb481e9fec49fbd4123cf854521
Andrei Bacescu <bacescuandrei@gmail.com>
2017-04-27 09:35:41 +0300

    Prototype Pattern

---
+++ src/main/java/net/metrosystems/designpattern/prototype/PrototypePatternDemo.java
@@ -0,0 +1,10 @@
+package net.metrosystems.designpattern.prototype;
+
+public class PrototypePatternDemo {
+	public static void main(String[] args){
+		ShapeCache.loadCache();
+		
+		Shape clonedShape = (Shape) ShapeCache.getShape("1");
+		System.out.println("Shape : " + clonedShape.getType());
+	}
+}
